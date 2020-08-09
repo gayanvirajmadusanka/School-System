7d7c50a0ba5b513cf138ef8ef13ea991a64845d5
Gayan Madusanka <gayan.virajs@gmail.com>
2020-08-09 11:10:20 +0530

    Created UI and controller

---
+++ src/lk/ijse/schoolsystem/controller/StudentForm.java
@@ -0,0 +1,27 @@
+package lk.ijse.schoolsystem.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class StudentForm {
+    public TextField txtId;
+    public TextField txtName;
+    public TextField txtAddress;
+    public TextField txtAge;
+    public TableColumn colId;
+    public TableColumn colName;
+    public TableColumn colAddress;
+    public TableColumn colAge;
+    public TableView tbl;
+
+    public void btnDeleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnUpdateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSaveOnAction(ActionEvent actionEvent) {
+    }
+}
